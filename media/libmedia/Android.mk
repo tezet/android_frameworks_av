f1b8aad33071d7a33e2908977ab7a19949f81194
Quarx2k <agent00791@gmail.com>
2012-07-14 19:08:54 +0700

    audio: Add compatibility bits to use legacy libaudio implementations (from ics)

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -57,6 +57,16 @@ ifeq ($(BOARD_USE_SAMSUNG_SEPARATEDSTREAM),true)
     LOCAL_CFLAGS += -DUSE_SAMSUNG_SEPARATEDSTREAM
 endif
 
+ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
+    LOCAL_SRC_FILES+= \
+        AudioParameter.cpp
+
+    LOCAL_CFLAGS += -DUSES_AUDIO_LEGACY
+    ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
+        LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
+    endif
+endif
+
 LOCAL_SHARED_LIBRARIES := \
 	libui libcutils libutils libbinder libsonivox libicuuc libexpat \
         libcamera_client libstagefright_foundation \
