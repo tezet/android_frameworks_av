13ca31bb9501d8bc44f5ccc1d8a439c1e653543b
nadlabak <pavel@doshaska.net>
2012-08-06 13:43:15 +0200

    OMXCodec: ifdef the qcom mess that breaks our camcorder
    Change-Id: I657a660b1ab211f37c1300efa0dd38028da1f1ce

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -3317,9 +3317,11 @@ void OMXCodec::drainInputBuffers() {
                 continue;
             }
 
+#ifdef QCOM_HARDWARE
             if (mIsEncoder && mIsVideo && (i == 4)) { //BIG TBD - move this to component
                 break;
             }
+#endif
 
             if (!drainInputBuffer(info)) {
                 break;
